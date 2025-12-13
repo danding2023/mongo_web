2724a1fff8f426fa08d55d6da54f1daeb0e70024
danding2023 <lisir5078@gmail.com>
2025-12-13 20:35:34 +0800

    Update all_zibeichaxun.js

--- js/all_zibeichaxun.js
+++ js/all_zibeichaxun.js
@@ -1,12 +1,18 @@
 fetch('../data/zibei.json')
-.then(res => res.json())
-.then(data => {
-    const tbody = document.getElementById('zibeiTable').querySelector('tbody');
+  .then(res => res.json())
+  .then(data => {
+    const tbody = document.querySelector('#zibeiTable tbody');
     tbody.innerHTML = '';
-    data.forEach(s => {
-        const tr = document.createElement('tr');
-        tr.innerHTML = `<td>${s.id}</td><td>${s.name}</td>`;
-        tbody.appendChild(tr);
+    data.forEach(item => {
+      const tr = document.createElement('tr');
+      tr.innerHTML = `
+        <td>${item.ID}</td>
+        <td>${item.聚集地}</td>`;
+      tbody.appendChild(tr);
     });
-})
-.catch(err => console.error('加载字辈数据失败:', err));
+  })
+  .catch(err => {
+    console.error('加载字辈数据失败:', err);
+    document.querySelector('#zibeiTable tbody').innerHTML =
+      '<tr><td colspan="2">数据加载失败，请稍后再试</td></tr>';
+  });
